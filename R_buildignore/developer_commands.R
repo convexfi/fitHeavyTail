72adc3ae1cd112f8bc649153582f1319d48dbf30
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-09-23 15:29:58 +0800

    Preparing details for CRAN submission.

--- R_buildignore/developer_commands.R
+++ R_buildignore/developer_commands.R
@@ -1,47 +1,46 @@
 ##
 ## User installation
 ##
-install.packages(file.choose(), repos = NULL, type="source")
+# Local installation
+install.packages(file.choose(), repos = NULL, type = "source")
+# Installation from GitHub
+devtools::install_github("dppalomar/covHeavyTail")
+# Installation from CRAN
+install.packages("covHeavyTail")
+# Getting help
 library(covHeavyTail)
-help(package="covHeavyTail")
+help(package = "covHeavyTail")
+package?covHeavyTail
 ?momentsStudentt
-
-
+citation("covHeavyTail")
+vignette(package = "covHeavyTail")
 
 
 ##
-## Developer commands (http://r-pkgs.had.co.nz/)
+## Developer commands (https://r-pkgs.org/)
 ##
-library(devtools)
-#devtools::create("covHeavyTail")
 devtools::load_all()  #or Ctrl-Shift-L
-#devtools::use_package("Gmedian")
-#devtools::use_package("mvtnorm")
-devtools::document()
 devtools::install()
-devtools::build()  # to generate the installation file
-#devtools::use_readme_rmd()  # to create the README file
-#devtools::use_data_raw()  # to set up the raw-data folder
+library(covHeavyTail)
 
-# code checking
-lintr::lint_package()
-devtools::check()
+# Documentation
+devtools::document()  #to generate all documentation via roxygen
+?covHeavyTail
 
-# code tests
-#devtools::use_testthat()
+
+# Code tests
 devtools::test()
-covr::package_coverage()  #coverage of tests
+#covr::package_coverage()  #coverage of tests
 
 
-# overall checks:
-#goodpractice::gp()
-
-# CRAN check
-rcmdcheck::rcmdcheck()
-#R CMD check --as-cran  # this is before submission to CRAN
-
-
-
-# to upload to CRAN or GitHub
-#devtools::install_github("yourusername/myfirstpackage")  # for GitHub
-#devtools::release()  #for CRAN
+# CRAN check and submission (https://r-pkgs.org/release.html)
+#  checklist: https://kalimu.github.io/post/checklist-for-r-package-submission-to-cran/
+devtools::check()  # run_dont_test = TRUE
+rcmdcheck::rcmdcheck()  # build_args = "--run-donttest"
+devtools::build()
+#devtools::revdep(pkg = "covHeavyTail")  # to check reverse dependencies
+#devtools::check_win_release()  #to check under windows
+#R CMD build .  # this is to generate tarball
+#R CMD check covHeavyTail_0.1.0.tar.gz --as-cran --run-donttest  # this is before submission to CRAN
+#R CMD install covHeavyTail_0.1.0.tar.gz
+#submit the tarball directly via the webform: https://cran.r-project.org/submit.html
