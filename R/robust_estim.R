d1f10111027870b7b4c321e760b1762db7e441fa
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-09-23 15:19:33 +0800

    Some changes.

--- R/robust_estim.R
+++ R/robust_estim.R
@@ -11,13 +11,13 @@
 # 5) Junyan: include shrinkage
 #
 
-# Use nv/(nv-2) as variable for checking convergence
+# Use nu/(nu-2) as variable for checking convergence
 
 
 inv <- function(...) solve(...)
 
 
-#' @importFrom Gmedian Gmedian
+#' @importFrom ICSNP spatial.median
 Gmedian_of_means <- function(X, k = 10) {
   T <- nrow(X)
   N <- ncol(X)
@@ -26,12 +26,12 @@ Gmedian_of_means <- function(X, k = 10) {
   mu <- matrix(NA, k, N)
   for (i in 1:k)
     mu[i, ] <- colMeans(X[i1[i]:i2[i], ])
-  mu <- as.vector( Gmedian::Gmedian(mu, init = colMeans(X)) )
+  mu <- ICSNP::spatial.median(X)
   return(mu)
 }
 
-#' @importFrom stats median
-#' @importFrom Gmedian Gmedian
+
+#' @importFrom ICSNP spatial.median
 gmean <- function(X, method = "mean", k = NULL) {
   switch(method,
          "mean" = {
@@ -41,7 +41,7 @@ gmean <- function(X, method = "mean", k = NULL) {
            return(apply(X, 2, median))
          },
          "Gmedian" = {
-           return(as.vector( Gmedian::Gmedian(X, init = colMeans(X)) ))
+           return(ICSNP::spatial.median(X))
          },
          "Gmedian of means" = {
            return(Gmedian_of_means(X, k))
@@ -92,7 +92,7 @@ momentsTyler <- function(X, verbose = FALSE) {
 
   #first, compute and remove mean
   mu <- gmean(X, "Gmedian of means", k = 10)
-  X_ <- X - rep(mu, each = T)
+  X_ <- X_ <- X - matrix(mu, T, N, byrow = TRUE)
 
   #second, compute covariance matrix up to a scaling factor with Tyler estimate
   Sigma <- cov(X_)  #Gaussian initial point
@@ -135,7 +135,7 @@ momentsTyler <- function(X, verbose = FALSE) {
 #' Estimation of mean and covariance matrix based on fitting data to the Cauchy distribution.
 #'
 #' The function \code{momentsCauchy} implements a maximum likelihood estimation (MLE) of the mean and covariance matrix
-#' to fit the data to a Cauchy distribution (Student-t with nv=1).
+#' to fit the data to a Cauchy distribution (Student-t with nu=1).
 #'
 #' @param X Data matrix containing the multivariate time series (each column is one time series).
 #' @param verbose If \code{TRUE}, info about iterations for convergence will be output.
@@ -182,11 +182,11 @@ momentsCauchy <- function(X, verbose = FALSE) {
     Sigma_prev <- Sigma
 
     #update
-    X_ <- X - rep(mu, each = T)
+    X_ <- X - matrix(mu, T, N, byrow = TRUE)
     weights <- 1/(1+rowSums(X_ * (X_ %*% inv(Sigma))))   # 1/( 1 + diag( X_ %*% inv(Sigma) %*% t(X_) ) )
     obj_value_record[k] <- - ((N+1)/2)*sum(log(weights)) + (T/2)*log(det(Sigma))
     mu <- as.vector(weights %*% X)/sum(weights)
-    X_ <- X - rep(mu, each = T)  #this could be removed...
+    X_ <- X - matrix(mu, T, N, byrow = TRUE)  #this could be removed...
     beta <- T/(N+1)/sum(weights)  #acceleration
     Sigma <- beta*((N+1)/T) * crossprod( sqrt(weights)*X_ )  # (N/T) * t(X_) %*% diag(weights) %*% X_
 
@@ -220,16 +220,16 @@ momentsCauchy <- function(X, verbose = FALSE) {
 #' Estimation of mean and covariance matrix based on fitting data to the Student-t distribution.
 #'
 #' The function \code{momentsStudentt} implements a maximum likelihood estimation (MLE) of the mean, covariance matrix,
-#' and degrees of freedom nv to fit the data to a Student-t distribution (via the ECME algorithm).
+#' and degrees of freedom nu to fit the data to a Student-t distribution (via the ECME algorithm).
 #'
 #' @param X Data matrix containing the multivariate time series (each column is one time series).
-#' @param nv If passed, then \code{nv} will not be optimized and will be set to that value.
-#' @param method Choice of method c("ECM", "ECME") for the maximization of \code{nv}.
+#' @param nu If passed, then \code{nu} will not be optimized and will be set to that value.
+#' @param method Choice of method c("ECM", "ECME") for the maximization of \code{nu}.
 #' @param verbose If \code{TRUE}, info about iterations for convergence will be output.
 #' @return A list with the following components:
 #' \item{\code{mu}}{mean estimate}
 #' \item{\code{cov}}{covariance matrix estimate}
-#' \item{\code{nv}}{degrees of freedom}
+#' \item{\code{nu}}{degrees of freedom}
 #' \item{\code{obj_value_record}}{convergence of objective value vs iterations}
 #' @author Daniel P. Palomar and Junyan LIU
 #' @references
@@ -245,9 +245,9 @@ momentsCauchy <- function(X, verbose = FALSE) {
 #' @export
 #' @importFrom stats cov var optimize
 #' @importFrom mvtnorm dmvt
-momentsStudentt <- function(X, nv = NA, method = "ECM", verbose = FALSE) {
+momentsStudentt <- function(X, nu = NULL, method = "ECM", verbose = FALSE) {
   max_iter <- 100
-  error_th_nv <- 0.1
+  error_th_nu <- 0.1
   error_th_Sigma <- 1e-3
   error_th_mu <- 1e-3
 
@@ -259,72 +259,76 @@ momentsStudentt <- function(X, nv = NA, method = "ECM", verbose = FALSE) {
   if (T == 1) stop("Only T=1 sample!!")
   if (N == 1) stop("Data is univariate!")
 
-  optimize_nv <- ifelse(is.na(nv), TRUE, FALSE)
-  if (!optimize_nv && nv == Inf)
-    nv <- 1e15
+  optimize_nu <- ifelse(is.null(nu), TRUE, FALSE)
+  if (!optimize_nu && nu == Inf)
+    nu <- 1e15
 
   #initial point based on sample mean and SCM
-  if (optimize_nv)
-    nv <- 10
+  if (optimize_nu)
+    nu <- 10
   mu <- colMeans(X)
-  Sigma <- (nv-2)/nv*cov(X)  #Sigma is the scale matrix, not the covariance matrix
+  Sigma <- (nu-2)/nu*cov(X)  #Sigma is the scale matrix, not the covariance matrix
 
   #loop
-  nv_record <- obj_value_record <- mu_diff_record <- Sigma_diff_record <- nv_diff_record <- rep(NA, max_iter)
+  nu_record <- obj_value_record <- mu_diff_record <- Sigma_diff_record <- nu_diff_record <- rep(NA, max_iter)
   for (k in 1:max_iter) {
     mu_prev <- mu
     Sigma_prev <- Sigma
-    nv_prev <- nv
+    nu_prev <- nu
 
-    #update
-    X_ <- X - rep(mu, each = T)
+    # update
+    X_ <- X - matrix(mu, T, N, byrow = TRUE)
     tmp <- rowSums(X_ * (X_ %*% inv(Sigma)))  # diag( X_ %*% inv(Sigma) %*% t(X_) )
-    obj_value_record[k] <- sum(mvtnorm::dmvt(X, delta = mu, sigma = Sigma, df = nv, log = TRUE, type = "shifted"))
-    #update mu and sigma
-    weights <- (nv+N)/(nv+tmp)  #E_tau
+    obj_value_record[k] <- sum(mvtnorm::dmvt(X, delta = mu, sigma = Sigma, df = nu, log = TRUE, type = "shifted"))
+    # update mu and sigma
+    weights <- (nu+N)/(nu+tmp)  #E_tau
     mu <- as.vector(weights %*% X)/sum(weights)
-    X_ <- X - rep(mu, each = T)  #this is slower: sweep(X, 2, FUN="-", STATS=mu)
+    X_ <- X - matrix(mu, T, N, byrow = TRUE)  #this is slower: sweep(X, 2, FUN = "-", STATS = mu)  #X_ <- X - rep(mu, each = TRUE)  # this is wrong?
     beta <- T/sum(weights)  #acceleration
-    Sigma <- beta*(1/T) * crossprod( sqrt(weights)*X_ )  # (1/T) * t(X_) %*% diag(weights) %*% X_
-    #update nv
-    if (optimize_nv) {
+    X_demeaned_gaussianized <- sqrt(beta) * sqrt(weights) * X_
+    Sigma <- (1/T) * crossprod(X_demeaned_gaussianized)  # (1/T) * t(X_) %*% diag(weights) %*% X_
+    # update nu
+    if (optimize_nu) {
       switch(method,
              "ECM" = {
-               # based on minus the Q function of nv
-               S <- T*(digamma((N+nv)/2) - log((N+nv)/2)) + sum(log(weights) - weights)  # S is E_log_tau-E_tau
-               Q_nv <- function(nv) { - T*(nv/2)*log(nv/2) + T*lgamma(nv/2) - (nv/2)*sum(S) }
-               nv <- optimize(Q_nv, interval = c(1e-6, 1e6))$minimum
+               # based on minus the Q function of nu
+               S <- T*(digamma((N+nu)/2) - log((N+nu)/2)) + sum(log(weights) - weights)  # S is E_log_tau-E_tau
+               Q_nu <- function(nu) { - T*(nu/2)*log(nu/2) + T*lgamma(nu/2) - (nu/2)*sum(S) }
+               nu <- optimize(Q_nu, interval = c(1e-6, 1e6))$minimum
              },
              "ECME" = {
-               # based on minus log-likelihood of nv with mu and sigma fixed to mu[k+1] and sigma[k+1]
+               # based on minus log-likelihood of nu with mu and sigma fixed to mu[k+1] and sigma[k+1]
                tmp <- rowSums(X_ * (X_ %*% inv(Sigma)))  # diag( X_ %*% inv(Sigma) %*% t(X_) )
-               LL_nv <- function(nv) { - sum ( - ((nv+N)/2)*log(nv+tmp) + lgamma( (nv+N)/2 ) - lgamma(nv/2) + (nv/2)*log(nv) ) }
-               nv <- optimize(LL_nv, interval=c(1e-6, 1e6))$minimum
+               LL_nu <- function(nu) { - sum ( - ((nu+N)/2)*log(nu+tmp) + lgamma( (nu+N)/2 ) - lgamma(nu/2) + (nu/2)*log(nu) ) }
+               nu <- optimize(LL_nu, interval=c(1e-6, 1e6))$minimum
              },
              stop("Method unknown")
       )
     }
 
     #stopping criterion
-    nv_record[k] <- nv
-    nv_diff_record[k] <- abs((nv - nv_prev)/nv_prev)
-    #nv_diff_record[k] <- abs((log(nv) - log(nv_prev))/log(nv_prev))
+    nu_record[k] <- nu
+    nu_diff_record[k] <- abs((nu - nu_prev)/nu_prev)
+    #nu_diff_record[k] <- abs((log(nu) - log(nu_prev))/log(nu_prev))
     mu_diff_record[k] <- norm(mu - mu_prev, "2")/norm(mu_prev, "2")
     Sigma_diff_record[k] <- norm(Sigma - Sigma_prev, "F")/norm(Sigma_prev, "F")
-    if (mu_diff_record[k] < error_th_mu &&
-        Sigma_diff_record[k] < error_th_Sigma &&
-        nv_diff_record[k] < error_th_nv)
+    if (mu_diff_record[k] < error_th_mu && Sigma_diff_record[k] < error_th_Sigma && nu_diff_record[k] < error_th_nu)
       break
   }
-  if (verbose)
-    cat(sprintf("Number of iterations for Student-t estimator = %d\n", k))
+  if (verbose) cat(sprintf("Number of iterations for Student-t estimator = %d\n", k))
   # print( figure(width=700, title="Convergence of parameters", xlab="t", ylab="param diff") %>%
-  #          ly_lines(na.omit(nv_diff_record), color="black", legend="nv") %>%
+  #          ly_lines(na.omit(nu_diff_record), color="black", legend="nu") %>%
   #          ly_lines(na.omit(mu_diff_record), color="blue", legend="mu") %>%
   #          ly_lines(na.omit(Sigma_diff_record), color="green", legend="Sigma") )
-  # figure(width=700, title="Convergence of parameters", xlab="t", ylab="nv") %>%
-  #   ly_lines(na.omit(nv_record), color="black", legend="nv")
+  # figure(width=700, title="Convergence of parameters", xlab="t", ylab="nu") %>%
+  #   ly_lines(na.omit(nu_record), color="black", legend="nu")
 
-  return( list(mu = as.vector(mu), cov = nv/(nv-2)*Sigma, nv = nv,
-               obj_value_record = na.omit(obj_value_record)) )
+  X_demeaned_gaussianized <- sqrt(nu/(nu - 2)) * X_demeaned_gaussianized
+  # note that norm((1/T)*t(X_demeaned_gaussianized) %*% X_demeaned_gaussianized - nu/(nu-2)*Sigma, "F")
+
+  names(mu) <- colnames(X)
+
+  return(list(mu = mu, cov = nu/(nu-2)*Sigma, scale = Sigma, nu = nu,
+              obj_value_record = na.omit(obj_value_record),
+              Xcg = X_demeaned_gaussianized))
 }
