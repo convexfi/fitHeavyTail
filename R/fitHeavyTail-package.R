fbf569b04463d0c1ee9ca48acbd195c8d831957c
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-09-27 22:43:13 +0800

    Added package info.

---
+++ R/fitHeavyTail-package.R
@@ -0,0 +1,23 @@
+#' fitHeavyTail: Mean and Covariance Matrix Estimation under Heavy Tails
+#'
+#' Estimation of mean vector and covariance matrix for heavy-tailed
+#' distributions. In particular, a multivariate Student t distribution is
+#' fitted to the data (possibly containing NAs).
+#' Special emphasis is placed on a fast implementation.
+#' The package is based on the paper:
+#' Rui ZHOU, Junyan Liu, Sandeep Kumar, and Daniel P. Palomar, "Robust factor
+#' analysis parameter estimation," Lecture Notes in Computer Science, 2020.
+#' <doi:10.1109/TSP.2015.2452219>
+#'
+#' @section Functions:
+#' \code{\link{momentsTyler}}, \code{\link{momentsCauchy}}, and \code{\link{momentsStudentt}}
+#'
+#' @section Help:
+#' For a quick help see the README file:
+#' \href{https://github.com/dppalomar/fitHeavyTail/blob/master/README.md}{GitHub-README}.
+#'
+#' @author Daniel P. Palomar and Rui ZHOU
+#'
+#' @docType package
+#' @name fitHeavyTail-package
+NULL
