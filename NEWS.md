3ac98bd609e3ac727d86a22a86fce74aad8c2745
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-09-27 11:44:48 +0800

    Changing name of package from covHeavyTail to fitHeavyTail.

--- NEWS.md
+++ NEWS.md
@@ -1,10 +1,10 @@
-## Changes in portfolioBacktest version 0.1.0.9000 (2019-XX-XX)
+## Changes in fitHeavyTail version 0.1.0.9000 (2019-XX-XX)
 
 * Initial release is on CRAN.
 
 * TBD
 
 
-## Changes in portfolioBacktest version 0.1.0 (2017-11-04)
+## Changes in fitHeavyTail version 0.1.0 (2017-11-04)
 
 * Initial bare-bone implementation (not publicly released).
--- R_buildignore/developer_commands.R
+++ R_buildignore/developer_commands.R
@@ -4,16 +4,16 @@
 # Local installation
 install.packages(file.choose(), repos = NULL, type = "source")
 # Installation from GitHub
-devtools::install_github("dppalomar/covHeavyTail")
+devtools::install_github("dppalomar/fitHeavyTail")
 # Installation from CRAN
-install.packages("covHeavyTail")
+install.packages("fitHeavyTail")
 # Getting help
-library(covHeavyTail)
-help(package = "covHeavyTail")
-package?covHeavyTail
+library(fitHeavyTail)
+help(package = "fitHeavyTail")
+package?fitHeavyTail
 ?momentsStudentt
-citation("covHeavyTail")
-vignette(package = "covHeavyTail")
+citation("fitHeavyTail")
+vignette(package = "fitHeavyTail")
 
 
 ##
@@ -21,11 +21,11 @@ vignette(package = "covHeavyTail")
 ##
 devtools::load_all()  #or Ctrl-Shift-L
 devtools::install()
-library(covHeavyTail)
+library(fitHeavyTail)
 
 # Documentation
 devtools::document()  #to generate all documentation via roxygen
-?covHeavyTail
+?fitHeavyTail
 
 
 # Code tests
@@ -38,9 +38,9 @@ devtools::test()
 devtools::check()  # run_dont_test = TRUE
 rcmdcheck::rcmdcheck()  # build_args = "--run-donttest"
 devtools::build()
-#devtools::revdep(pkg = "covHeavyTail")  # to check reverse dependencies
+#devtools::revdep(pkg = "fitHeavyTail")  # to check reverse dependencies
 #devtools::check_win_release()  #to check under windows
 #R CMD build .  # this is to generate tarball
-#R CMD check covHeavyTail_0.1.0.tar.gz --as-cran --run-donttest  # this is before submission to CRAN
-#R CMD install covHeavyTail_0.1.0.tar.gz
+#R CMD check fitHeavyTail_0.1.0.tar.gz --as-cran --run-donttest  # this is before submission to CRAN
+#R CMD install fitHeavyTail_0.1.0.tar.gz
 #submit the tarball directly via the webform: https://cran.r-project.org/submit.html
