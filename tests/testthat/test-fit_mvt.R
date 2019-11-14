025ebaf1b13ad3efa661e5359f61ea59be4556e8
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-11-14 22:22:14 +0800

    Revising tests

--- tests/testthat/test-fit_mvt.R
+++ tests/testthat/test-fit_mvt.R
@@ -1,59 +1,94 @@
-context("Checking function \"fit_mvt()\"")
+context("Function \"fit_mvt()\"")
 
-# generate the multivariate Student's t data ----------------
+# # generate the multivariate Student's t data ----------------
 # set.seed(123)
 # N <- 10
 # T <- 50
 # nu <- 6
 # X <- mvtnorm::rmvt(n = T, sigma = diag(N), df = nu, delta = rep(0, N))
-# save(X, file = "tests/testthat/X.RData", version = 2, compress = "xz")
+# colnames(X) <- c(1:10)
+# save(X, file = "X.RData", version = 2, compress = "xz")
 
+library(testthat)
+library(fitHeavyTail)
 load("X.RData")
 
-# default mode --------------------------------------------------------------------------------------------------------
-test_that("test the default mode", {
+test_that("error control works", {
+
+  expect_error(fit_mvt(X = median), "\"X\" must be a matrix or coercible to a matrix.")
+
+  expect_error(fit_mvt(X = "HongKong"), "\"X\" only allows numerical or NA values.")
+
+  expect_error(fit_mvt(X = 1), "Only T=1 sample!!")
+
+  expect_error(fit_mvt(X = X, factors = -1), "\"factors\" must be no less than 1 and no more than column number of \"X\".")
+
+  expect_error(fit_mvt(X = X, max_iter = -1), "\"max_iter\" must be greater than 1.")
+
+  expect_error(fit_mvt(X = X, nu = "lala"), "Non-valid value for nu.")
+
+  expect_error(fit_mvt(X = X, nu = 1), "Non-valid value for nu.")
+
+  expect_error(fit_mvt(X = X, nu_regcoef = 0.1, nu_target = 1), "Non-valid value for nu_target.")
+})
+
+
+test_that("default mode works", {
   # mvt_model_check <- fit_mvt(X)
-  # save(mvt_model_check, file = "tests/testthat/mvt_model_check.RData", version = 2, compress = "xz")
+  # save(mvt_model_check, file = "mvt_model_check.RData", version = 2, compress = "xz")
   mvt_model <- fit_mvt(X)
+
+  #TODO{Rui}: You can see the problem here:
+  #mvt_model <- fit_mvt(X, return_iterates = TRUE)
+  #plotConvergence(mvt_model)
+
+
   load("mvt_model_check.RData")
   expect_equal(mvt_model, mvt_model_check)
 })
 
-# factor structure constraint -----------------------------------------------------------------------------------------
-test_that("with factor structure constraint on scatter matrix", {
-  # mvt_model_factor_check <- fit_mvt(X, 5)
-  # save(mvt_model_factor_check, file = "tests/testthat/mvt_model_factor_check.RData", version = 2, compress = "xz")
-  mvt_model_factor <- fit_mvt(X, 5)
+
+test_that("Gaussian case fits", {
+  mvt_model <- fit_mvt(X, nu = Inf)
+  expect_equal(mvt_model$mu, colMeans(X))
+  expect_equal(mvt_model$cov, (nrow(X)-1)/nrow(X) * cov(X))
+})
+
+
+test_that("factor structure constraint on scatter matrix works", {
+  # mvt_model_factor_check <- fit_mvt(X, factors = 5)
+  # save(mvt_model_factor_check, file = "mvt_model_factor_check.RData", version = 2, compress = "xz")
+  mvt_model_factor <- fit_mvt(X, factors = 5)
   load("mvt_model_factor_check.RData")
   expect_equal(mvt_model_factor, mvt_model_factor_check)
 })
 
-# missing data in X ---------------------------------------------------------------------------------------------------
-test_that("X contains missing values", {
+
+test_that("X with NAs works", {
   X_wNA <- X
   for (i in 1:5) X_wNA[i, i] <- NA
-  # mvt_model_missing_check <- fit_mvt(X_wNA)
-  # save(mvt_model_missing_check, file = "tests/testthat/mvt_model_missing_check.RData", version = 2, compress = "xz")
-  mvt_model_missing <- fit_mvt(X_wNA)
-  load("mvt_model_missing_check.RData")
-  expect_equal(mvt_model_missing, mvt_model_missing_check)
+  # mvt_model_wNA_check <- fit_mvt(X_wNA)
+  # save(mvt_model_wNA_check, file = "mvt_model_wNA_check.RData", version = 2, compress = "xz")
+  mvt_model_wNA <- fit_mvt(X_wNA)
+  load("mvt_model_wNA_check.RData")
+  expect_equal(mvt_model_wNA, mvt_model_wNA_check)
 })
 
-# fix nu --------------------------------------------------------------------------------------------------------------
-test_that("fix nu", {
+
+test_that("fixed nu works", {
   # nu_kurtosis <- fitHeavyTail:::est_nu_kurtosis(X)
-  # mvt_model_fixnu_check <- fit_mvt(X, nu = nu_kurtosis)
-  # save(mvt_model_fixnu_check, file = "tests/testthat/mvt_model_fixnu_check.RData", version = 2, compress = "xz")
-  load("mvt_model_fixnu_check.RData")
-  mvt_model_fixnu <- fit_mvt(X, nu = "kurtosis")
-  expect_equal(mvt_model_fixnu, mvt_model_fixnu_check)
+  # mvt_model_fixednu_check <- fit_mvt(X, nu = nu_kurtosis)
+  # save(mvt_model_fixednu_check, file = "mvt_model_fixednu_check.RData", version = 2, compress = "xz")
+  load("mvt_model_fixednu_check.RData")
+  mvt_model_fixednu <- fit_mvt(X, nu = "kurtosis")
+  expect_equal(mvt_model_fixednu, mvt_model_fixednu_check)
 })
 
-# regularize nu -------------------------------------------------------------------------------------------------------
-test_that("regularize nu", {
+
+test_that("regularized nu works", {
   # nu_kurtosis <- fitHeavyTail:::est_nu_kurtosis(X)
   # mvt_model_regnu_check <- fit_mvt(X, nu_target = nu_kurtosis, nu_regcoef = 1)
-  # save(mvt_model_regnu_check, file = "tests/testthat/mvt_model_regnu_check.RData", version = 2, compress = "xz")
+  # save(mvt_model_regnu_check, file = "mvt_model_regnu_check.RData", version = 2, compress = "xz")
   load("mvt_model_regnu_check.RData")
   mvt_model_regnu <- fit_mvt(X, nu_regcoef = 1)
   expect_equal(mvt_model_regnu, mvt_model_regnu_check)
